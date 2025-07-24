ed54afed7716128b832688ab1abeb8ef40e788ed
Lina <linamered3@gmail.com>
2025-07-23 23:35:24 -0400

    Added another text + textfield for age and variables for age and textAge

--- InteractiveUI/ContentView.swift
+++ InteractiveUI/ContentView.swift
@@ -10,10 +10,16 @@ import SwiftUI
 struct ContentView: View {
     @State private var name : String = ""
     
+    @State private var age : String = ""
+    
     @State private var textTitle = "What is your name?"
     
+    @State private var textAge = "What is your age?"
+    
+    @State private var presentAlert = false
+    
     var body: some View {
-        ZStack {
+        VStack {
             Text(textTitle)
                 .font(.title)
             
@@ -21,8 +27,18 @@ struct ContentView: View {
                 .multilineTextAlignment(.center)
                 .font(.title)
             
-            Button("Sumbit name") {
+            Text(textAge)
+                .font(.title)
+            
+            TextField("Type your age here...", text:$age)
+                .multilineTextAlignment(.center)
+                .font(.title)
+
+            
+            Button("Submit") {
+                presentAlert = true
                 textTitle = "Welcome, \(name)!"
+                textAge = "Age: \(age)"
             }
             .font(.title2)
             .buttonStyle(.borderedProminent)
